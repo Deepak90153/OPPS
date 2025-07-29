7eaa099d26f46d30473ce80fe3ce7d789a1e2051
Deepak Botla <2420090153@klh.edu.in>
2025-07-29 11:54:25 +0530

    Add files via upload

---
+++ Practical/src/Task_1/Typeconversion.java
@@ -0,0 +1,10 @@
+package Task_1;
+
+public class Typeconversion {
+    public static void main(String[] args) {
+        int num=25;
+        double converted=num;
+        System.out.println("Integer value: " +num);
+        System.out.println("Converted to double: "+converted);
+    }
+}
